530a0a05644195f9d06472e1fe9dce449c1520ce
Aravindhan Elayakumar <aravindhansai@gmail.com>
2018-12-12 18:31:04 -0800

    Adding login page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def login():
+    return render_template('page-login.html')
 
 
 if __name__ == '__main__':
